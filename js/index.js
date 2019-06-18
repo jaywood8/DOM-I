af050380d7c3b98f5c354bf8f15ec401d4e6ed5e
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-06-17 21:43:57 -0700

    Achieved MVP

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const siteContent = {
     "img-src": "img/logo.png"
   },
   "cta": {
-    "h1": "DOM Is Awesome",
+    "h1": "DOM \n Is \n Awesome",
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
@@ -28,7 +28,7 @@ const siteContent = {
   },
   "contact": {
     "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
+    "address" : "123 Way 456 Street \n Somewhere, USA",
     "phone" : "1 (888) 888-8888",
     "email" : "sales@greatidea.io",
   },
@@ -47,20 +47,63 @@ codeSnippet.setAttribute("src", siteContent["cta"]["img-src"])
 let middleImage = document.getElementById("middle-img");
 middleImage.setAttribute("src", siteContent["main-content"]["middle-img-src"])
 
-const navText = document.querySelectorAll("header nav a");
+let navText = document.querySelectorAll("header nav a");
 navText[0].textContent = siteContent["nav"]["nav-item-1"];
 navText[1].textContent = siteContent["nav"]["nav-item-2"];
 navText[2].textContent = siteContent["nav"]["nav-item-3"];
 navText[3].textContent = siteContent["nav"]["nav-item-4"];
 navText[4].textContent = siteContent["nav"]["nav-item-5"];
 navText[5].textContent = siteContent["nav"]["nav-item-6"];
+navText[0].style.color = "green";
+navText[1].style.color = "green";
+navText[2].style.color = "green";
+navText[3].style.color = "green";
+navText[4].style.color = "green";
+navText[5].style.color = "green";
 
 const ctaText = document.querySelector(".cta-text h1");
-ctaText.innerHTML = "DOM<br> Is<br> Awesome";
+ctaText.textContent = siteContent["cta"]["h1"];
 
 const btnText = document.querySelector(".cta-text button");
 btnText.textContent = siteContent["cta"]["button"];
-// document.querySelector(".cta-text h1").style.whiteSpace = 'pre';
+document.querySelector(".cta-text h1").style.whiteSpace = 'pre';
 
-const featuresHeader = document.querySelectorAll("h4");
-featuresHeader.textContent[0] = siteContent["main-content"]["features-h4"];
+const topContentHeaders = document.querySelectorAll(".text-content h4");
+topContentHeaders[0].textContent = siteContent["main-content"]["features-h4"];
+topContentHeaders[1].textContent = siteContent["main-content"]["about-h4"];
+topContentHeaders[2].textContent = siteContent["main-content"]["services-h4"];
+topContentHeaders[3].textContent = siteContent["main-content"]["product-h4"];
+topContentHeaders[4].textContent = siteContent["main-content"]["vision-h4"];
+
+const topContentText = document.querySelectorAll(".text-content p");
+topContentText[0].textContent = siteContent["main-content"]["features-content"];
+topContentText[1].textContent = siteContent["main-content"]["about-content"];
+topContentText[2].textContent = siteContent["main-content"]["services-content"];
+topContentText[3].textContent = siteContent["main-content"]["product-content"];
+topContentText[4].textContent = siteContent["main-content"]["vision-content"];
+
+const contactHeader = document.querySelectorAll(".contact h4");
+contactHeader[0].textContent = siteContent["contact"]["contact-h4"];
+
+const contactText = document.querySelectorAll(".contact p");
+contactText[0].textContent = siteContent["contact"]["address"];
+contactText[1].textContent = siteContent["contact"]["phone"];
+contactText[2].textContent = siteContent["contact"]["email"];
+contactText[0].style.whiteSpace = 'pre-line';
+
+const footerText = document.querySelectorAll("footer");
+footerText[0].textContent = siteContent["footer"]["copyright"];
+
+let newNavNode1 = document.createElement("a");                 
+let newNavText1 = document.createTextNode("Appended Item");         
+newNavNode1.appendChild(newNavText1);                              
+document.querySelector("nav").appendChild(newNavNode1);  
+
+console.log(newNavNode1);
+console.log("these are the a tags", document.querySelector("a"))
+console.log("these are the nav tags", document.querySelector("nav"))
+
+let newNavNode2 = document.createElement("a");                 
+let newNavText2 = document.createTextNode("Prepended Item");         
+newNavNode2.prepend(newNavText2);                              
+document.querySelector("nav").prepend(newNavNode2);  
