e50f756699336743ea45c1c90fff802df81875b8
Jay Wood <47437698+jaywood8@users.noreply.github.com>
2019-06-17 22:28:04 -0700

    Cleaned up code, added for loop for turning links green.

--- js/index.js
+++ js/index.js
@@ -47,6 +47,7 @@ codeSnippet.setAttribute("src", siteContent["cta"]["img-src"])
 let middleImage = document.getElementById("middle-img");
 middleImage.setAttribute("src", siteContent["main-content"]["middle-img-src"])
 
+//Updating Nav content
 let navText = document.querySelectorAll("header nav a");
 navText[0].textContent = siteContent["nav"]["nav-item-1"];
 navText[1].textContent = siteContent["nav"]["nav-item-2"];
@@ -54,20 +55,34 @@ navText[2].textContent = siteContent["nav"]["nav-item-3"];
 navText[3].textContent = siteContent["nav"]["nav-item-4"];
 navText[4].textContent = siteContent["nav"]["nav-item-5"];
 navText[5].textContent = siteContent["nav"]["nav-item-6"];
-navText[0].style.color = "green";
-navText[1].style.color = "green";
-navText[2].style.color = "green";
-navText[3].style.color = "green";
-navText[4].style.color = "green";
-navText[5].style.color = "green";
 
+//Turn nav text to green
+// navText[0].style.color = "green";
+// navText[1].style.color = "green";
+// navText[2].style.color = "green";
+// navText[3].style.color = "green";
+// navText[4].style.color = "green";
+// navText[5].style.color = "green";
+
+//Trying for loop to assign nav text
+// Array.from(navText).map((i) => {
+//   navText[i].textContent = siteContent["nav"]["key"];
+// })
+
+
+
+
+//Updating CTA
 const ctaText = document.querySelector(".cta-text h1");
 ctaText.textContent = siteContent["cta"]["h1"];
 
+//Button
 const btnText = document.querySelector(".cta-text button");
 btnText.textContent = siteContent["cta"]["button"];
 document.querySelector(".cta-text h1").style.whiteSpace = 'pre';
 
+
+//Main Content Headers
 const topContentHeaders = document.querySelectorAll(".text-content h4");
 topContentHeaders[0].textContent = siteContent["main-content"]["features-h4"];
 topContentHeaders[1].textContent = siteContent["main-content"]["about-h4"];
@@ -75,6 +90,7 @@ topContentHeaders[2].textContent = siteContent["main-content"]["services-h4"];
 topContentHeaders[3].textContent = siteContent["main-content"]["product-h4"];
 topContentHeaders[4].textContent = siteContent["main-content"]["vision-h4"];
 
+//Main Content Paras
 const topContentText = document.querySelectorAll(".text-content p");
 topContentText[0].textContent = siteContent["main-content"]["features-content"];
 topContentText[1].textContent = siteContent["main-content"]["about-content"];
@@ -82,6 +98,7 @@ topContentText[2].textContent = siteContent["main-content"]["services-content"];
 topContentText[3].textContent = siteContent["main-content"]["product-content"];
 topContentText[4].textContent = siteContent["main-content"]["vision-content"];
 
+//Contact
 const contactHeader = document.querySelectorAll(".contact h4");
 contactHeader[0].textContent = siteContent["contact"]["contact-h4"];
 
@@ -91,19 +108,29 @@ contactText[1].textContent = siteContent["contact"]["phone"];
 contactText[2].textContent = siteContent["contact"]["email"];
 contactText[0].style.whiteSpace = 'pre-line';
 
+//Footer
 const footerText = document.querySelectorAll("footer");
 footerText[0].textContent = siteContent["footer"]["copyright"];
 
+//Pre-Appending Items
 let newNavNode1 = document.createElement("a");                 
 let newNavText1 = document.createTextNode("Appended Item");         
 newNavNode1.appendChild(newNavText1);                              
 document.querySelector("nav").appendChild(newNavNode1);  
 
-console.log(newNavNode1);
-console.log("these are the a tags", document.querySelector("a"))
-console.log("these are the nav tags", document.querySelector("nav"))
-
 let newNavNode2 = document.createElement("a");                 
 let newNavText2 = document.createTextNode("Prepended Item");         
 newNavNode2.prepend(newNavText2);                              
 document.querySelector("nav").prepend(newNavNode2);  
+
+//Testing...
+// console.log(newNavNode1);
+// console.log("these are the a tags", document.querySelector("a"))
+// console.log("these are the nav tags", document.querySelector("nav"))
+
+
+//Using  forLoop to turn nav items green
+
+navText.forEach((link) => {
+  link.style.color = "green";
+})
